8f5175c87f3d5f4c32bb4319d7c50ade8592037f
Gabriel R. Marques <gabriel.r.marques@ufv.br>
2024-08-09 17:58:16 -0300

    .

--- Verilog/Datapath.v
+++ Verilog/Datapath.v
@@ -55,31 +55,30 @@ module Datapath (
     input wire clk, reset
 );
 
-    wire [31:0] PCOut;
+    wire [31:0] PC;
     wire [31:0] instruction;
+    wire [31:0] immediate;
+    wire [31:0] readData1, readData2, readData3;
+    wire [31:0] muxout0, muxout1, muxout2;
+    wire [31:0] addout0, addout1;
     wire Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
     wire [2:0] ALUOp;
-    wire [31:0] readData1, readData2;
-    wire [31:0] immediate;
     wire [3:0] ALUControl;
-    wire [31:0] MUXOut0, MUXOut1, MUXOut2;
     wire [31:0] ALUResult;
     wire zero;
-    wire [31:0] readData;
-    wire [31:0] addout0, addout1;
 
-    Add add0(PCOut, 4, addout0);
-    Add add1(PCOut, immediate, addout1);
-    ALU alu(readData1, MUXOut0, ALUControl, ALUResult, zero);
+    Add add0(PC, 4, addout0);
+    Add add1(PC, immediate, addout1);
+    ALU alu(readData1, muxout0, ALUControl, ALUResult, zero);
     ALUControl alucontrol(instruction[14:12], ALUOp, ALUControl);
     Control control(instruction[6:0], Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
-    DataMemory datamemory(MemWrite, MemRead, ALUResult, readData2, readData);
+    DataMemory datamemory(MemWrite, MemRead, ALUResult, readData2, readData3);
     ImmGen immgen(instruction, immediate);
-    InstructionMemory instructionmemory(PCOut, instruction);
-    Mux mux0(ALUSrc, readData2, immediate, MUXOut0);
-    Mux mux1(Branch & zero, addout0, addout1, MUXOut1);
-    Mux mux2(MemtoReg, ALUResult, readData, MUXOut2);
-    PC pc(clk, reset, MUXOut1, PCOut);
-    Registers registers(RegWrite, instruction[19:15], instruction[24:20], instruction[11:7], MUXOut2, readData1, readData2);
+    InstructionMemory instructionmemory(PC, instruction);
+    Mux mux0(ALUSrc, readData2, immediate, muxout0);
+    Mux mux1(Branch & zero, addout0, addout1, muxout1);
+    Mux mux2(MemtoReg, ALUResult, readData3, muxout2);
+    PC pc(clk, reset, muxout1, PC);
+    Registers registers(RegWrite, instruction[19:15], instruction[24:20], instruction[11:7], muxout2, readData1, readData2);
 
 endmodule
